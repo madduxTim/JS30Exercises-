2074820ba7c0e4f09f91c25fca5257c4f09840a2
Tim Maddux <timothymaddux@gmail.com>
2017-03-22 14:01:52 -0500

    finished

--- js/event-capture.js
+++ js/event-capture.js
@@ -1,1 +1,19 @@
-console.log("blah");
+const divs = document.querySelectorAll("div");
+const button = document.querySelector("button");
+
+function logText(e) {
+	console.log(this.classList.value);
+	// e.stopPropagation(); // stop bubbling from happening, so parents are not consoled
+	// console.log(this);
+}
+
+divs.forEach(div => div.addEventListener("click", logText, {
+	capture: false,
+	once: true
+}));
+
+button.addEventListener("click", () => {
+	console.log("click happened!");
+}, {
+	once: true
+});
